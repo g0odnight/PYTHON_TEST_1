af61172535f3727228ccdc9425a0a3a26120e19e
Edgard Protosevicius <edgard.protosevicius@gmail.com>
2022-12-28 22:13:39 +0200

    Fifth task done

--- Task 5/main.py
+++ Task 5/main.py
@@ -1,11 +1,16 @@
 # Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
-
+from modules.math.composition import composition as addition
+from modules.math.division import division as divivsion
+from modules.math.multiplication import *
+from modules.math.subtraction import *
+from modules.numbers.numbers import *
 
 # Kitų failų ir žemiau esančio kodo nekeiskite
 a = addition(one, four);
 b = divivsion(four, two);
 c = substraction(three, two);
 d = multiplication(five, two);
+
 
 print(a);
 print(b);
